ae7534a48d3183ec2ca0fb45d08cc0bc38974f11
Tien Pham <r3dsky02@yahoo.com.vn>
2018-04-24 21:52:11 +0300

    [server] Root query

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
-const app = express()
+const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  
-}))
+app.use("/graphql", graphqlHTTP({
+  schema
+}));
 
 app.listen(4000, () => {
-  console.log('now listening at 4000')
-})
+  console.log("now listening at 4000");
+});
