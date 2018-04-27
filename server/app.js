1b97aa3ccb6b04583cdd920f01c93cc8a71a4061
Tien Pham <r3dsky02@yahoo.com.vn>
2018-04-27 21:41:36 +0300

    [server] connect to mlab, add mongoose

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
+const { username, password } = require("./env");
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+mongoose.connect(
+  `mongodb://${username}:${password}@ds159489.mlab.com:59489/gql-react-project`
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to db");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
   console.log("now listening at 4000");
