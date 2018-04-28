4a8928744736e1d7acbb340da6f195f83b736a9c
Tien Pham <r3dsky02@yahoo.com.vn>
2018-04-28 13:47:27 +0300

    [server] mutation for adding books

--- server/models/author.js
+++ server/models/author.js
@@ -6,4 +6,4 @@ const authorSchema = new Schema({
   age: Number
 });
 
-module.exports = mongoose.model("Book", authorSchema);
+module.exports = mongoose.model("Author", authorSchema);
--- server/models/book.js
+++ server/models/book.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const authorSchema = new Schema({
+const bookSchema = new Schema({
   name: String,
-  age: Number
+  genre: String,
+  authorId: String
 });
 
-module.exports = mongoose.model("Author", authorSchema);
+module.exports = mongoose.model("Book", bookSchema);
