1b09ab2de451f10aaccdd54dbe759e2b52c078e8
Tien Pham <r3dsky02@yahoo.com.vn>
2018-04-28 13:08:00 +0300

    [server] Mutation working with mlab

--- server/models/book.js
+++ server/models/book.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+const authorSchema = new Schema({
   name: String,
-  genre: String,
-  authorId: String
+  age: Number
 });
 
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model("Author", authorSchema);
