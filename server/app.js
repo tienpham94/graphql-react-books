a86f604752eaad59bd11acbbb1ab96eb033d3fc4
Tien Pham <r3dsky02@yahoo.com.vn>
2018-04-28 18:56:23 +0300

    [server] Add cors

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const { username, password } = require("./env");
 const app = express();
+
+app.use(cors);
 
 mongoose.connect(
   `mongodb://${username}:${password}@ds159489.mlab.com:59489/gql-react-project`
